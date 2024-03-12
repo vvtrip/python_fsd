04ac9f34e0bda41825c80c2946c45cb6d4e82664
Vaibhav Tripathi <vaibhavvtrip@gmail.com>
2024-03-12 10:07:24 +0530

    adds name mangling

--- python_oops/03_encapsulation_and_abstraction/02_car_encapsulated.py
+++ python_oops/03_encapsulation_and_abstraction/02_car_encapsulated.py
@@ -2,9 +2,9 @@ class Car:
 
     def __init__(self, make, model, year):
 
-        self.make = make
-        self.model = model
-        self._year = year
+        self.__make = make # name mangling
+        self.__model_ = model # name mangling
+        self._year = year  # recommended convention
 
 
 my_car = Car('Hyundai', 'i10', 2018)
@@ -31,3 +31,22 @@ print(my_car.year)
 
 # __year can also be created and the technique is called name mangling
 # but is supposed to be used in special cases
+
+print(my_car._Car__make)
+print(my_car._Car__model_)
+# There is no concept of private in python, rather a soft 
+# non public concept is present and achieved through compliance 
+# with convention. It is used to avoid accessing some attributes 
+# outside of class.
+
+# Name Mangling is a special case of non public attribute where two 
+# leading underscore or an additional lagging underscore is added to 
+# attribute within class. outside of the class the attribute can still 
+# be accessed but by even complex naming convention of _Classname__attribute. 
+# Thus inceasing the complexity of using the attribute.
+
+# It decreases the readability and ease of testing, thus only a single 
+# leading underscore is recommended to be added before an attribute by convention
+
+# two leading and two trailing underscore do not make name mangling 
+# rather it is a convention for magic function like __init__
