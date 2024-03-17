5b4a2b1db668823dd947e16d9310e5903b114664
Vaibhav Tripathi <vaibhavvtrip@gmail.com>
2024-03-17 15:15:49 +0530

    adds comment on name mangling over methods

--- python_oops/04_methods/03_call_one_method_from_another.py
+++ python_oops/04_methods/03_call_one_method_from_another.py
@@ -20,3 +20,15 @@ my_backpack = Backpack()
 my_backpack.add_multiple_items(['Bottle', 'Bag'])
 
 print(my_backpack.items)
+
+# Non-Public Methods
+# To follow the Python naming conventions, to make a method "non-public", 
+# you should add a leading underscore to its name, like this:
+
+# def _add_items:
+
+# Name Mangling
+# Adding two underscores to the name of the method will trigger the 
+# process of name mangling:
+
+# def __add_items:
