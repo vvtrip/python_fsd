214e941f894ab3598ed49df9527bd2a81737e39a
Vaibhav Tripathi <vaibhavvtrip@gmail.com>
2024-04-03 09:26:57 +0530

    adds Player class

--- python_oops/05_dice_game_project/02_dice_game.py
+++ python_oops/05_dice_game_project/02_dice_game.py
@@ -15,8 +15,46 @@ class Die:
         self._value = new_value
         return self._value
 
-my_die = Die()
-print(my_die.value)
+class Player:
 
-my_die.roll()
+    def __init__(self, die, is_computer=False):
+        #if not sure whether to make attributes public or non public, always choose non public
+        #as recommended by python style guide
+        self._die = die
+        self._is_computer = is_computer
+        self._counter = 10
+
+    @property
+    def die(self):
+        return self._die
+    
+    @property
+    def is_computer(self):
+        return self._is_computer
+
+    @property
+    def counter(self):
+        return self._counter
+    
+    def roll_die(self):
+        self._die.roll()
+
+    def increment_counter(self):
+        self._counter += 1
+
+    def decrement_counter(self):
+        self._counter -= 1
+
+
+my_die = Die()
+player = Player(my_die, is_computer=False)
+print(my_die.value) #None
+
+my_die.roll() #same value for below 2 prints
+print(my_die.value) 
+print(player.die.value)
+
+player.roll_die() #same value for below 2 prints
 print(my_die.value)
+print(player.die.value)
+
